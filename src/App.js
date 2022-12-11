ba023e890f49e4a15dc3116fdc0a300b1af2f0f5
RiOAOn <rian0422@naver.com>
2022-12-11 13:46:14 +0900

    css 초안 작성

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import React, { useState } from 'react';
 import './App.css';
 import Template from './components/Template';
 import TodoList from './components/TodoList';
+import { MdAddCircleOutline, MdAddCircle, MdRealEstateAgent } from 'react-icons/md';
 
 const App = () => {
   const [todos, setTodos] = useState([
     {
       id: 1,
       text: '할일1',
-      checkde: true,
+      checked: true,
+    },
+
+    {
+      id: 2,
+      text: '할일2',
+      checked: false,
     },
   ]);
+
   return (
     <div>
-      <Template>
+      <Template todoLength={todos.length}>
         <TodoList todos={todos} />
+        <div className="add_todo_button">
+          <MdAddCircle />
+        </div>
       </Template>
     </div>
   );
